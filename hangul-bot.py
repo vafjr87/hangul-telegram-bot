80cf513ab9912460ed23497875c56eff291b88df
Virgílio Fernandes Jr. @ virgiliofernandes.me <vafjr87@gmail.com>
2017-06-17 04:37:10 -0300

    Improvements :D
    + Now @hangul_bot really replies your messages!
    + It teaches you how to use the commands when you send it wrong or in a bad format
    + Error logs for better service

--- hangul-bot.py
+++ hangul-bot.py
@@ -1,92 +1,111 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import datetime as dt
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import botlog
 import logging
 import naver as n
 import pickle
 import romanizator as r
 
-lang_errors = {
+command_errors = {
     'ko': {
-        'en': 'Use /english <message>',
-        'zh-CN': 'Use /korean_chinese <message>'
+        'en': 'Use /english <text>',
+        'zh-CN': 'Use /korean_chinese <text>'
     },
-    'en': {'ko': 'Use /korean <message>'},
-    'zh-CN': {'ko': 'Use /chinese_korean <message>',
-    'romanize': 'Use /romanize <message>'}
+    'en': {'ko': 'Use /korean <text>'},
+    'zh-CN': {'ko': 'Use /chinese_korean <text>',
+    'romanize': 'Use /romanize <text>'}
 }
 
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"' % (update, error))
+
+
 def start(bot, update):
-    start_message = "🇰🇷 Welcome to the Hangul Bot 🇰🇷\n\n"
-    start_message += "I can help you with romanization and translations\n\n\n"
+    botlog.log_activity(update)
+    start_message = "🇰🇷 Welcome to the Hangul Bot, {} 🇰🇷\n\n".format(update.message.chat.first_name)
+    start_message += "I can help you with romanization and translations!\n\n\n"
     # start_message += "🇰🇷 한글봇에 오신걸 환영합니다 🇰🇷\n\n"
     # start_message += "제가 로마자 표기법과 통역을 도와 드릴수 있어요!\n"
-    print(update)
-    dir(update)
     bot.send_message(chat_id=update.message.chat_id, text=start_message)
 
 
 def romanize(bot, update, args):
+    botlog.log_activity(update)
     message = ' '.join(args)
     romanizator = r.Romanizator()
 
     if message == '':
-        message = lang_errors['romanize']
+        message = command_errors['romanize']
     else:
         if (romanizator.has_hangul(message)):
             message = romanizator.romanize(message)
         else:
             message = "There's no hangul in this message 🤔"
 
-    bot.send_message(chat_id=update.message.chat_id, text=message)
+    update.message.reply_text(message, quote=True)
 
 
 def translate(source, target, text):
     if text == '':
-        return lang_errors[source][target]
+        return command_errors[source][target]
 
     naver = n.Naver()
     return naver.translate(source, target, text)
 
 
 def english(bot, update, args):
+    botlog.log_activity(update)
     message = ' '.join(args)
-    update.message.reply_text(translate('ko', 'en', message))
+    update.message.reply_text(translate('ko', 'en', message), quote=True)
 
 
 def korean(bot, update, args):
+    botlog.log_activity(update)
     message = ' '.join(args)
-    update.message.reply_text(translate('en', 'ko', message))
+    update.message.reply_text(translate('en', 'ko', message), quote=True)
 
 
 def chinese_korean(bot, update, args):
+    botlog.log_activity(update)
     message = ' '.join(args)
-    update.message.reply_text(translate('zh-CN', 'ko', message))
+    update.message.reply_text(translate('zh-CN', 'ko', message), quote=True)
 
 
 def korean_chinese(bot, update, args):
+    botlog.log_activity(update)
     message = ' '.join(args)
-    update.message.reply_text(translate('ko', 'zh-CN', message))
+    update.message.reply_text(translate('ko', 'zh-CN', message), quote=True)
 
 
 def echo(bot, update):
+    botlog.log_activity(update)
     romanizator = r.Romanizator()
 
     if (romanizator.has_hangul(update.message.text)):
         message = romanizator.romanize(update.message.text)
-        update.message.reply_text(message)
+        update.message.reply_text(message, quote=True)
 
 
 def unknown(bot, update):
-    text = """Sorry, I didn't understand your command! Are you a North Korean spy?! 🇰🇵\
+    botlog.log_activity(update)
+    message = """Sorry, I didn't understand your command! Are you a North Korean spy?! 🇰🇵\
     \n\nhttps://youtu.be/EFwitVDo540"""
-    bot.send_message(chat_id=update.message.chat_id, text=text)
+    update.message.reply_text(message, quote=True)
 
 
 def help(bot, update):
-    help_message = """
+    botlog.log_activity(update)
+    message = """
 Hello! I am the Hangul Bot! 🇰🇷
 
 How can I help you now?
@@ -104,38 +123,42 @@ Other commands:
 
 /romanize <text>:  romanize Hangul sentences
 
-If you send me any text within Hangul, I will romanize it.
+If you send me any message within Hangul, I will romanize it.
 
 Wait for news!
 
-If you have any questions or suggestions or money to give 💰, ping @vafjr87."""
+If you have any questions or suggestions – or money to give 💰–, ping my <b>beloved master</b> @vafjr87"""
 
-    bot.send_message(chat_id=update.message.chat_id, text=help_message)
+    update.message.reply_text(message, quote=True)
 
 
-if __name__ == '__main__':
+def main():
     with open('token', 'rb') as token:
         token = pickle.load(token)
 
     updater = Updater(token=token.get('telegram'))
-    dispatcher = updater.dispatcher
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('romanize', romanize, pass_args=True))
+    dp.add_handler(CommandHandler('english', english, pass_args=True))
+    dp.add_handler(CommandHandler('English', english, pass_args=True))
+    dp.add_handler(CommandHandler('Korean', korean, pass_args=True))
+    dp.add_handler(CommandHandler('korean', korean, pass_args=True))
+    dp.add_handler(CommandHandler('chinese_korean', chinese_korean, pass_args=True))
+    dp.add_handler(CommandHandler('Chinese_korean', chinese_korean, pass_args=True))
+    dp.add_handler(CommandHandler('korean_chinese', korean_chinese, pass_args=True))
+    dp.add_handler(CommandHandler('Korean_chinese', korean_chinese, pass_args=True))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
 
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('romanize', romanize, pass_args=True))
-    dispatcher.add_handler(CommandHandler('english', english, pass_args=True))
-    dispatcher.add_handler(CommandHandler('English', english, pass_args=True))
-    dispatcher.add_handler(CommandHandler('Korean', korean, pass_args=True))
-    dispatcher.add_handler(CommandHandler('korean', korean, pass_args=True))
-    dispatcher.add_handler(CommandHandler('chinese_korean', chinese_korean, pass_args=True))
-    dispatcher.add_handler(CommandHandler('Chinese_korean', chinese_korean, pass_args=True))
-    dispatcher.add_handler(CommandHandler('korean_chinese', korean_chinese, pass_args=True))
-    dispatcher.add_handler(CommandHandler('Korean_chinese', korean_chinese, pass_args=True))
-    dispatcher.add_handler(CommandHandler('help', help))
+    dp.add_error_handler(error)
 
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    print('@hangul_bot is running! 한글 봇 만세! ')
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == '__main__':
+    print('Start Time:\t{}\t@hangul_bot is running! 한글 봇 만세! '.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
+    main()
+    print('End Time:\t{}\t@hangul_bot is running! 한글 봇 만세! '.format(dt.now().strftime("%Y-%m-%d %H:%M:%S")))
